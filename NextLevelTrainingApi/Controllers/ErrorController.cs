abe43ceeeb85fee61d77839318c819cc52104989
Sercan Dağlı <sercandagli@Sercan-MacBook-Pro.local>
2020-10-24 21:35:54 +0300

    #create error log api added

---
+++ NextLevelTrainingApi/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NextLevelTrainingApi.ViewModels;
+using NextLevelTrainingApi.DAL.Interfaces;
+using NextLevelTrainingApi.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NextLevelTrainingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ErrorController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpPost]
+        [Route("CreateErrorLog")]
+        public IActionResult Error(CreateErrorViewModel errorVm)
+        {
+            ErrorLog error = new ErrorLog();
+            error.Id = Guid.NewGuid();
+            error.Exception = errorVm.Exception;
+            error.StackTrace = errorVm.StackTrace;
+            error.CreatedDate = DateTime.Now;
+            _unitOfWork.ErrorLogRepository.InsertOne(error);
+            return Ok();
+        }
+    }
+}
